435b36aecfc8e839b764e48af6a131a4f471b5b9
雷华康 <1340984030@qq.com>
2018-02-14 20:21:04 +0800

    过年前测试无误
    放心上传

---
+++ PopupWindow/gen/cn/itcast/popwindow/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cn.itcast.popwindow;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ singleInstance/gen/cn/itcast/singleinstance/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cn.itcast.singleinstance;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
